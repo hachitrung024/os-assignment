4c907aa08ebfe1c116d692b1d7641fc14774deae
trungha <hachitrung024@gmail.com>
2024-11-13 13:49:01 +0700

    update queue.c

--- src/queue.c
+++ src/queue.c
@@ -9,12 +9,31 @@ int empty(struct queue_t * q) {
 
 void enqueue(struct queue_t * q, struct pcb_t * proc) {
         /* TODO: put a new process to queue [q] */
+        if(q->size >= MAX_QUEUE_SIZE) return;   // Hang doi day
+        q->proc[q->size++] = proc;              // Them process vao queue
 }
 
 struct pcb_t * dequeue(struct queue_t * q) {
         /* TODO: return a pcb whose prioprity is the highest
          * in the queue [q] and remember to remove it from q
          * */
-	return NULL;
+        if(empty(q)) return NULL;
+        struct pcb_t * target_proc = NULL;   // pcb can deqeueu
+        uint8_t target_index = 0;       // Vi tri cua pcb
+        uint32_t min_priority_val = q->proc[0]->priority; // gia tri priorty nho nhat
+        // Tim proc co gia tri priority nho nhat
+        for(uint8_t i = 0; i < q->size; i++){
+                if(q->proc[i]->priority < min_priority_val) {
+                        min_priority_val = q->proc[i]->priority;
+                        target_index = i;
+                }
+        }
+        target_proc = q->proc[target_index];    // pcb tra ve
+        // Xoa pcb khoi queue
+        for(int i = target_index; i< q->size -1; i++){
+                q->proc[i] = q->proc[i+1];
+        }
+        q->size --;
+        return target_proc;
 }
 
